6a273f4087bf672b119dd930abca3e5213504644
max <m.evsyukov@2gis.ru>
2017-09-26 13:41:25 +0700

    first commit2

---
+++ WorkCalendarWeb/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {HeroService} from './services/calendars.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  values: string[] = ["123"];
+
+  constructor(private heroService: HeroService) {
+    this.heroService
+      .getVaules()
+      .then(values => {
+        console.log(values);
+        this.values = values as string[]; });
+  }
+
+
+}
---
+++ WorkCalendarWeb/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { AppComponent } from './app.component';
+import {HeroService} from "./services/calendars.service";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule, HttpModule
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
