9634e65cece832807cbce6252ed5f2d73c88391b
Евсюков Максим (m.evsyukov) <m.evsyukov@2gis.ru>
2017-09-28 17:34:58 +0700

    ВВОд

--- WorkCalendarWeb/src/app/services/calendars.service.ts
+++ WorkCalendarWeb/src/app/services/calendars.service.ts
@@ -20,16 +20,6 @@ export class HeroService {
       .catch(this.handleError);
   }
 
-
-  // getHero(id: number): Promise<Hero> {
-  //   const url = `${this.heroesUrl}/${id}`;
-  //   return this.http.get(url)
-  //     .toPromise()
-  //     .then(response => response.json().data as Hero)
-  //     .catch(this.handleError);
-  // }
-
-
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
